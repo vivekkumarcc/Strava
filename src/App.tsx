f383088ad4c6001ad6edaf664d0cedc0fa38a5de
Vivek Kumar <vivekk9039@gmail.com>
2024-10-09 00:21:41 +0530

    work done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes";
+import "./App.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-main">
+      <RouterProvider router={routes} fallbackElement={<div>Loading...</div>} />
+      <ToastContainer />
     </div>
   );
 }
